a31a7534609f3fa0a0e91387bfdb7160ae97feff
Kizito Azubuike <kizito.azubuike1@gmail.com>
2023-05-28 13:58:08 +0100

    Update Clock.jsx

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -10,7 +10,7 @@ const Clock = () => {
     let interval
 
     const countDown = () => {
-      const destination = new Date('January 31, 2023').getTime()
+      const destination = new Date('January 31, 2024').getTime()
       interval = setInterval(() => {
         const now = new Date().getTime()
         const difference = destination - now
